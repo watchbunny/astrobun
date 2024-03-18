693369464ba218bec1dadcff8665a45d0d0df457
watchbunny <rawlins.emmerson@gmail.com>
2024-03-18 13:11:55 -0700

    s

--- Bunny/Bunny.gd
+++ Bunny/Bunny.gd
@@ -1,23 +1,37 @@
 extends CharacterBody2D
 class_name Bunny
 
-@export var speed = 300
-@export var gravity = 30
-@export var jump_force = 300
+@export var speed = 200
+@export var jump_height = -400
+
+@onready var bunny = $"."
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	if !is_on_floor():
-		velocity.y += gravity
-		if velocity.y > 1000:
-			velocity.y = 1000
+	if not is_on_floor():
+		velocity.y += gravity * delta
 	
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = -jump_force
-	var horizontal_direction = Input.get_axis("move_left", "move_right")
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_height
+		
+	var horizontal_direction = Input.get_axis("left", "right")
+	if horizontal_direction:
+		velocity.x = speed * horizontal_direction
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
 	
-	velocity.x = speed * horizontal_direction
-	
+	#if horizontal_direction>0:
+		#bunny.flip_h=false
+		#RayCast2D.scale.x=-.5
+		#
+	#elif horizontal_direction<0:
+		#bunny.flip_h=true
+		#RayCast2D.scale.x=.5
+		
+		
+		
 	move_and_slide()
 	
-	print(velocity)
+	
 
---
+++ enemy/enemy.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+class_name Enemy
+
+var speed = -60
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var facing_right = false
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if $RayCast2D.is_colliding() && is_on_floor():
+		flip()
+	
+	velocity.x = speed
+	move_and_slide()
+	
+func flip():
+	facing_right = !facing_right
+	
+	scale.x = abs(scale.x) * -1
+	if facing_right:
+		speed = abs(speed)
+	else:
+		speed = abs(speed) * -1
